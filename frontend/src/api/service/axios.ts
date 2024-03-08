5236085e6c8e1d5d493d1671d4e37fad8f7385ef
Luiz <luizpmonteiro8@hotmail.com>
2024-03-08 20:13:15 -0300

    axios frontend timeout change

--- frontend/src/api/service/axios.ts
+++ frontend/src/api/service/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const Axios = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  timeout: 10000,
+  timeout: 60000,
   timeoutErrorMessage: "Tempo limite excedido.",
   withCredentials: true,
   headers: {
